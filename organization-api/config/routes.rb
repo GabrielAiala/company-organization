d4c2d0e39824c04acec8ce3746c54bd010a29f30
Gabriel <gabriel.aiala@hotmail.com>
2024-09-21 16:35:20 -0300

    fix graphql gem

--- organization-api/config/routes.rb
+++ organization-api/config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  post "/graphql",          to: "graphql/base#execute"
-  get  "/graphiql",         to: "graphql/base#graphiql"
-  get  "/graphql/describe", to: "graphql/base#describe"
+  post "/graphql", to: "graphql#execute"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
