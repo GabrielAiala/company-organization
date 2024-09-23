3c64df0842119241be4d2194db64c51d637edc09
Gabriel <gabriel.aiala@hotmail.com>
2024-09-23 12:41:58 -0300

    added configs and theme

---
+++ organization-app/src/theme/globalStyles.ts
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: ${(props) => props.theme.fonts.main};
+    background-color: ${(props) => props.theme.colors.base};
+    color: ${(props) => props.theme.colors.text};
+    line-height: 1.6;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`;
+
+export default GlobalStyle;
---
+++ organization-app/src/theme/lightTheme.ts
@@ -0,0 +1,12 @@
+export const lightTheme = {
+  colors: {
+    primary: "#384B70",
+    secundary: "#507687",
+    base: "#FCFAEE",
+    dange: "#B8001F",
+    text: "#00000",
+  },
+  fonts: {
+    main: "Arial"
+  }
+}
